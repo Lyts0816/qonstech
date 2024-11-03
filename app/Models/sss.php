21e31506d368852d08151cefc70b4c1a81c77b61
Machys <134110423+Lyts0816@users.noreply.github.com>
2024-11-04 07:16:39 +0800

    0

--- app/Models/sss.php
+++ app/Models/sss.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class sss extends Model
+class Sss extends Model
 {
     use HasFactory;
 
