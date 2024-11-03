f37ddea4e98d9fca45d374b714f889655a9b10a3
Machys <134110423+Lyts0816@users.noreply.github.com>
2024-11-04 07:24:25 +0800

    1

--- app/Models/pagibig.php
+++ app/Models/pagibig.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class pagibig extends Model
+class Pagibig extends Model
 {
     use HasFactory;
 
