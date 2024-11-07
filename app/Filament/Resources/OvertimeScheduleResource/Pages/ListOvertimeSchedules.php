8ff575ab64f3fdad4c4ad48fc52ffda2deef0912
Machys <134110423+Lyts0816@users.noreply.github.com>
2024-11-07 09:50:54 +0800

    07

--- app/Filament/Resources/OvertimeScheduleResource/Pages/ListOvertimeSchedules.php
+++ app/Filament/Resources/OvertimeScheduleResource/Pages/ListOvertimeSchedules.php
@@ -4,6 +4,8 @@ namespace App\Filament\Resources\OvertimeScheduleResource\Pages;
 
 use App\Filament\Resources\OvertimeScheduleResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
+use App\Models\OvertimeSchedule;
 use Filament\Resources\Pages\ListRecords;
 
 class ListOvertimeSchedules extends ListRecords
@@ -14,6 +16,23 @@ class ListOvertimeSchedules extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'activeEmployees' => Tab::make('Active Overtime')
+            ->badge(OvertimeSchedule::whereNull('deleted_at')->count()) 
+            ->modifyQueryUsing(function ($query) {
+                $query->whereNull('deleted_at'); 
+            }),
+
+            'archive' => Tab::make('Deactivated Overtime')
+            ->badge(OvertimeSchedule::onlyTrashed()->count())
+            ->modifyQueryUsing(function ($query) {
+                $query->onlyTrashed();
+            }),
         ];
     }
 }
